134a916f2a7aa0a6bb4bb1630a93bb84fb2ab909
Amarjeet Singh <amargifty7@gmail.com>
2025-03-15 21:59:17 +0530

    Updates Node code

--- 3-getting-started/1-executing-scripts/app.js
+++ 3-getting-started/1-executing-scripts/app.js
@@ -1,1 +1,32 @@
-console.log(module);
+// console.log(module);
+// console.log('Hello World!');
+
+// setTimeout();
+// clearTimeout();
+// setInterval();
+// clearInterval();
+
+var message = 'test';
+// window.message;
+// console.log(global.message);
+// console.log(message);
+// console.log(window.message);
+
+var sayHello = function() {
+    console.log('Hello');
+}
+
+// sayHello();
+
+function sayHi() {
+    console.log('Hi');
+}
+
+// sayHi();
+
+const logger = require('./logger');
+
+// console.log(logger);
+// logger.log('message');
+
+logger('message');
